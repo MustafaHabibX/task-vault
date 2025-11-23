9e0a16c0c17fca05108bad6219af1a728d336c7d
MustafaHabibX <136514653+MustafaHabibX@users.noreply.github.com>
2025-11-23 10:20:08 +0430

    feat: implement job service
    Added getAllJobs service to fetch jobs for a specific user
    
    Added createJob service with Prisma integration
    
    Handled optional status mapping using Prisma enum JobStatus

---
+++ src/services/job-service.js
@@ -0,0 +1,52 @@
+import { JobStatus } from "@prisma/client";
+import prisma from "../database/prisma.js";
+
+export async function getAllJobs(userID) {
+  try {
+    const jobs = await prisma.job.findMany({ where: { userId: userID } });
+    if (!jobs) {
+      return { status: 404, message: "Error getting jobs" };
+    }
+
+    return { status: 200, message: "Request Successfully procced.", jobs };
+  } catch (err) {
+    console.error("Error occured when fetching jobs.", err);
+    return {
+      status: 400,
+      message: "Error occured when fetching jobs.",
+      jobs: null,
+    };
+  }
+}
+
+// Temporary implementation
+export async function createJob(
+  userID,
+  inputName,
+  inputDescription,
+  inputStatus
+) {
+  try {
+    console.log(inputStatus);
+    const status =
+      inputStatus && JobStatus[inputStatus.toUpperCase()]
+        ? JobStatus[inputStatus.toUpperCase()]
+        : undefined;
+
+    const result = await prisma.job.create({
+      data: {
+        name: inputName,
+        description: inputDescription,
+        status: status,
+        user: {
+          connect: { id: userID },
+        },
+      },
+    });
+
+    return { status: 200 };
+  } catch (err) {
+    console.error("An error occured creating a new job.", err);
+    return { status: 400 };
+  }
+}
